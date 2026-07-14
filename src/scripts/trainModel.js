ca5f0fb091b62e161bb17ed491f0d10dcfe1c585
Ishan Meduri <vasu.ishu.meduri@gmail.com>
2026-07-14 15:52:37 +0530

    Add data normalization and tensor conversion for TensorFlow.js training

--- src/scripts/trainModel.js
+++ src/scripts/trainModel.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import * as tf from "@tensorflow/tfjs";
 
 const rawData = fs.readFileSync("trainingData.json","utf-8");
 const rawdataset = JSON.parse(rawData);
@@ -19,3 +20,32 @@ const maxZscore = Math.max(...zscore);
 function normalFunction(value,min,max){
     return ((value -min)/ (max-min)); 
 }
+
+const normalizedDataset = dataset.map(example => {
+  const normalizedZscore = normalFunction(example.features.zscore, minZscore, maxZscore);
+  const normalizedAmount = normalFunction(example.features.amount, minAmount, maxAmount);
+  const noveltyAsNumber = +example.features.noveltyRisk;
+  const timeAnomalyAsNumber = +example.features.timeAnomalyRisk;
+
+  return {
+    features: [normalizedZscore, noveltyAsNumber, timeAnomalyAsNumber, normalizedAmount],
+    label: example.label,
+  };
+});
+
+console.log(normalizedDataset[0]);
+
+const featureArrays = normalizedDataset.map(example => example.features);
+const labels = normalizedDataset.map(example => example.label);
+
+
+console.log(featureArrays[0]);
+console.log(labels[0]);
+
+
+// Tensor Part
+const featureTensor = tf.tensor2d(featureArrays);
+const labelTensor = tf.tensor2d(labels,[labels.length,1]);
+
+featureTensor.print();
+labelTensor.print();
